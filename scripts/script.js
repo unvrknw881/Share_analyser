4a2e1517dd694e7fa1a717cc1a1ea6e656498fa7
unvrknw881 <58877074+unvrknw881@users.noreply.github.com>
2021-08-23 15:20:17 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -30,52 +30,83 @@ const shareAnalyser = function (e) {
     
     
     if (numberOfShares > 1)
-    {
-     if (investedSharePrice < finalSharePrice) {
-      showMessage(
+  {
+     
+     if (investedSharePrice < finalSharePrice)
+     
+       {
+     
+       showMessage(
         `You have invested in ${numberOfShares} shares at ₹${investedSharePrice}, now the final price is ₹${finalSharePrice} & you have made a profit of ₹${
           (finalSharePrice - investedSharePrice) * numberOfShares
         }`,
         'green'
       );
-    } else if (investedSharePrice > finalSharePrice) {
-      showMessage(
+       
+     } 
+      
+      else if (investedSharePrice > finalSharePrice) 
+      {
+         showMessage(
         `You have invested in ${numberOfShares} shares at ₹${investedSharePrice}, now the final price is ₹${finalSharePrice} & you have made a loss of ₹${
           (investedSharePrice - finalSharePrice) * numberOfShares
         }`,
         'red'
       );
-    } else {
+      
+      } 
+      
+      else 
+         {
+           
       showMessage('You have no profit no loss', 'black');
-    }
-  } else {
-    showMessage('Enter all the values, and make sure that number of shares purchased should be atleast one.', 'red');
-  }
-}
-  else if (numberOfShares == 1){
+        
+         }
   
-    if (investedSharePrice < finalSharePrice) {
-      showMessage(
+    } 
+    else 
+    {
+    showMessage('Enter all the values, and make sure that number of shares purchased should be atleast one.', 'red');
+    }
+ }
+  
+  else if (numberOfShares == 1)
+  {
+  
+       if (investedSharePrice < finalSharePrice) 
+       {
+          showMessage(
         `You have invested in ${numberOfShares} share at ₹${investedSharePrice}, now the final price is ₹${finalSharePrice} & you have made a profit of ₹${
           (finalSharePrice - investedSharePrice) * numberOfShares
         }`,
         'green'
       );
-    } else if (investedSharePrice > finalSharePrice) {
+       
+       } 
+    
+      else if (investedSharePrice > finalSharePrice) 
+       {
       showMessage(
         `You have invested in ${numberOfShares} share at ₹${investedSharePrice}, now the final price is ₹${finalSharePrice} & you have made a loss of ₹${
           (investedSharePrice - finalSharePrice) * numberOfShares
         }`,
         'red'
       );
-    } else {
+      
+       } 
+    
+    else 
+      {
       showMessage('You have no profit no loss', 'black');
-    }
-  } else {
+      
+      }
+  } 
+  else 
+  {
     showMessage('Enter all the values, and make sure that number of shares purchased should be atleast one.', 'red');
   }
   
-  }
+}
   
 };
 
